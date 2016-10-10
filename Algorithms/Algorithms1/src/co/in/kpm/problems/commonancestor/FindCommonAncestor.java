cf6a5b7b144b39a98064f6b43297e2886e465a1f
Krishna Murthy Mirajkar <kpmirajkar@gmail.com>
2016-10-10 14:32:10 +0530

    Reset repo

--- Algorithms/Algorithms1/src/co/in/kpm/problems/commonancestor/FindCommonAncestor.java
+++
@@ -1,5 +0,0 @@
-package co.in.kpm.problems.commonancestor;
-
-public interface FindCommonAncestor {
-  String findCommmonAncestor(String[] commitHashes, String[][] parentHashes, String commitHash1, String commitHash2);
-}
